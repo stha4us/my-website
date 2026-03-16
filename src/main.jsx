8bb4a070969191d7e331056d980765c0d134cbf9
stha4us <utsab199@gmail.com>
2026-03-16 22:30:51 +1100

    Page refresh persitance option added.

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ root.render(
   <React.StrictMode>
     {/* <HashRouter> */}
         {/* Catch all routes and render App component */}
-        <BrowserRouter>
+        <BrowserRouter basename = "/stha4us.github.io/">
           {/* <Route path="*" element={<App />} /> */}
           <App />
         </BrowserRouter>
