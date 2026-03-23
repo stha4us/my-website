f1ac490ce049f4a033d82cb9c6ff20a79a1c1d00
stha4us <utsab199@gmail.com>
2026-03-23 19:46:10 +1100

    Backend components removed.

--- backend/homepage/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Homepage
-
-class HomepageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Homepage
-        fields = ('id', 'title', 'description', 'enabled')
